f664308610ec36f8f73388aa594992a913c8ff96
ShevchSM <96664054+ShevchSM@users.noreply.github.com>
2021-12-26 11:09:27 +0200

    Initial_commit

---
+++ 001/Homework_001.py
@@ -0,0 +1,44 @@
+first = 10
+second = 30
+
+result = first + second     # сумма
+print('+ ', result)
+print(type(result))
+
+result = first - second     # разница
+print('- ', result)
+print(type(result))
+
+result = first * second     # умножение
+print('* ', result)
+print(type(result))
+
+result = first / second     # деление
+print('/ ', result)
+print(type(result))
+
+result = first ** second    # возведение в степень
+print('** ', result)
+print(type(result))
+
+result = first // second    # целочисленное деление
+print('// ', result)
+print(type(result))
+
+result = first % second     # остаток от деления
+print('% ', result)
+print(type(result))
+
+# Ответы на вопросы
+
+question1 = 'Результат, ожидаемый от прохождения этого курса?'
+answer1 = 'Понимание программ написанных с использованием Python, а также написание своих программ!!!'
+print(question1,' - ', answer1)
+
+question2 = 'Как будет использоваться диплом?'
+answer2 = 'Как возможность перехода в другой отдел предприятия!!!'
+print(question2,' - ', answer2)
+
+question3 = 'Чем Я готов пожертвовать для получения результат?'
+answer3 = 'Свободным временем!!!'
+print(question3,' - ', answer3)
